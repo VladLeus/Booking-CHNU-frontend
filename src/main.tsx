cce78d18d247333b99f9df8e58eb7524416a0b57
VladLeus <vladyslavleus@gmail.com>
2024-05-22 11:38:13 +0300

    Created typescript aliases to optimize imports.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './shared/store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
