5460c5a975da3d9163250da5a1f158501e32091d
VladLeus <vladyslavleus@gmail.com>
2024-05-23 12:56:41 +0300

    Improved date range picker.

--- src/ui/forms/DateTimePicker.tsx
+++ src/ui/forms/DateTimePicker.tsx
@@ -5,6 +5,8 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import { SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
 import { Dayjs } from 'dayjs';
+import 'dayjs/locale/uk';
+import Calendar from '@mui/icons-material/Event';
 
 interface Props {
   startText: string;
@@ -15,9 +17,19 @@ interface Props {
 
 const DateTimePicker: FC<Props> = ({ startText, endText, value, setValue }) => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'uk'}>
       <DateRangePicker
+        disablePast
+        sx={{
+          width: 'auto',
+        }}
         slots={{ field: SingleInputDateRangeField }}
+        slotProps={{
+          textField: {
+            placeholder: `${startText} - ${endText}`,
+            InputProps: { startAdornment: <Calendar sx={{ mr: 1 }} /> },
+          },
+        }}
         localeText={{ start: startText, end: endText }}
         value={value}
         onChange={(newValue) => {
