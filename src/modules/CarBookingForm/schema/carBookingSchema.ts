dea1034d28cd7266548db8c5bd45cd599355be38
VladLeus <vladyslavleus@gmail.com>
2024-06-10 13:55:50 +0300

    Fixed that user can set past date to book a car.

--- src/modules/CarBookingForm/schema/carBookingSchema.ts
+++ src/modules/CarBookingForm/schema/carBookingSchema.ts
@@ -4,21 +4,31 @@ import dayjs from 'dayjs';
 
 import dayJsSchema from '@modules/RegistrationForm/schema/dayjsSchema.ts';
 
+const currentDate = dayjs();
+
 export const carBookingSchema = z
   .object({
     firstName: z.string().min(2, "Обов'язкове поле"),
     city: z.string().min(1, "Обов'язкове поле"),
     start_date: dayJsSchema
       .nullable()
-      .refine((data) => data !== null, 'Оберіть початок оренди'),
+      .refine((data) => data !== null, 'Оберіть початок оренди')
+      .refine(
+        (data) => dayjs(data).isAfter(currentDate),
+        'Дата початку не може бути в минулому',
+      ),
     end_date: dayJsSchema
       .nullable()
-      .refine((data) => data !== null, 'Оберіть кінець оренди'),
+      .refine((data) => data !== null, 'Оберіть кінець оренди')
+      .refine(
+        (data) => dayjs(data).isAfter(currentDate),
+        'Дата закінчення не може бути в минулому',
+      ),
   })
   .refine(
     (data) => {
       if (data.start_date && data.end_date) {
-        return dayjs(data.end_date).isAfter(dayjs(data.start_date));
+        return dayjs(data.end_date).isAfter(dayjs(data.start_date), 'day');
       }
       return true;
     },
@@ -30,12 +40,12 @@ export const carBookingSchema = z
   .refine(
     (data) => {
       if (data.start_date && data.end_date) {
-        return dayjs(data.start_date).isBefore(dayjs(data.end_date));
+        return dayjs(data.start_date).isBefore(dayjs(data.end_date), 'day');
       }
       return true;
     },
     {
-      message: 'Дата початку має раніше дати закінчення',
+      message: 'Дата початку має бути раніше дати закінчення',
       path: ['start_date'],
     },
   );
